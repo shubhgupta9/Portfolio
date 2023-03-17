226f30ade91b85a29f533de673e5a5acf6c20a5e
Shubh Gupta <shubh.g0279@gmail.com>
2023-03-17 13:21:08 +0530

    Image Updates About

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,6 @@ function Hero({pageInfo}: Props) {
         <img 
         className='realtive rounded-full h-32 w-32 mx-auto object-cover'
          src={urlFor(pageInfo?.heroImage).url()}/>
-         {/* src="https://pps.whatsapp.net/v/t61.24694-24/299448188_381211900755151_6745586857079759047_n.jpg?ccb=11-4&oh=01_AVwQRoLmdR1t-bpx9DSNyoz8VoBquLPpznVYsVIYVduU5g&oe=63434D4B" alt="" /> */}
          <div className='z-20'>
           <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>IT Undergrad</h2>
         <h1 className='text-xl lg:text-6xl font-semibold px-10'>
