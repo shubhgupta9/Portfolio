af841af4db560e266f13b3797ad1774d3d97bb9e
Shubh Gupta <shubh.g0279@gmail.com>
2023-03-17 13:15:31 +0530

    Hero Updated

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,13 +2,6 @@ import { PageInfo } from "../typings";
 import axios from "axios";
 
 export const fetchPageInfo = async () => {
-  console.log(process.env);
-  console.log(
-    `${process.env.VERCEL_ENV === "production" ? "https" : "http"}://${
-      process.env.NEXT_PUBLIC_VERCEL_URL
-    }/api/getPageInfo`
-  );
-
   const { data } = await axios.get(
     `${process.env.VERCEL_ENV === "production" ? "https" : "http"}://${
       process.env.NEXT_PUBLIC_VERCEL_URL
